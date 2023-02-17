f1ee55fbc469c17ac817e7ed10e07b7292b6162a
I C-AN <chicori3@naver.com>
2023-02-17 23:52:50 +0900

    @Around 적용

---
+++ src/test/java/hello/springaop/aop/AopTest.java
@@ -0,0 +1,42 @@
+package hello.springaop.aop;
+
+import hello.springaop.order.OrderRepository;
+import hello.springaop.order.OrderService;
+import hello.springaop.order.aop.AspectV1;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+@SpringBootTest
+@Import(AspectV1.class)
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
